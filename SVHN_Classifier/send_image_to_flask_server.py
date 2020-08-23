f76d9bdc926082f9e334343bbc93b55ef4264cd5
rcv1k4s <vikaschamarthi240@gmail.com>
2020-08-23 16:19:30 -0400

    send_image_to_flask_server.py Script to send a single image from local to the server

---
+++ SVHN_Classifier/send_image_to_flask_server.py
@@ -0,0 +1,13 @@
+import requests
+
+def send_image(ip,port,image_name):
+    url = 'http://{}:{}/im_size'.format(ip, port)
+    my_img = {'image': open(image_name, 'rb')}
+    ret = requests.post(url, files=my_img)
+    print(ret.content)
+
+if __name__ == '__main__':
+    ip = '0.0.0.0'
+    port = 7718
+    image_name = 'test_input.png'
+    send_image(ip,port,image_name)
